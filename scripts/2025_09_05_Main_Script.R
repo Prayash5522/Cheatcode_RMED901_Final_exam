284fa7115faa84a684467a6dee4858647e54ef7a
OSMANELRAYAH <osman.osma@uib.no>
2025-09-05 14:25:01 +0200

    seperat gender variable

--- scripts/2025_09_05_Main_Script.R
+++ scripts/2025_09_05_Main_Script.R
@@ -12,7 +12,8 @@ original_data <- read_delim(here("data", "2025_09_05_original_exam_data.txt"))
 
 # I seperated the gender_arm variable into two variables , one of them contain the gender and the other contain the arm
 # delete the gender arm 
+
 original_data <- original_data %>% 
-  +     mutate(arm = sub(".*_", "", gender_arm)) %>%
-  +     select(-gender_arm)
-> original_data
+      mutate(arm = sub(".*_", "", gender_arm)) %>%
+      select(-gender_arm) 
+      original_data 
