1e0b33b4bde3177b20ce1d83f97f0d5e02407e71
OSMANELRAYAH <osman.osma@uib.no>
2025-09-08 14:28:12 +0200

    The merged data

--- scripts/2025_09_05_Main_Script.R
+++ scripts/2025_09_05_Main_Script.R
@@ -123,5 +123,30 @@ tidy_data_day_2 <- read_delim(here("data", "2025_09_08_tidy_version_day2.txt"))
 
 tidy_data_day_2 <- tidy_data_day_2 %>% select(-year, -month, -baseline_esr_cat)
 
+glimpse(tidy_data_day_2)
+
+##I removed the duplicates from the tidy_data_day_2 using the patient_id coloumn
+
+tidy_data_day_2 <- tidy_data_day_2 %>%
+  distinct(patient_id, .keep_all = TRUE)
+
+##import and assign the additional data set
+Additiona_data <- read_delim(here("data", "2025_09_05_original_exam_data_join.txt"))
+
+## arrang both data sets tidy_data_day_2 and the Additiona_data by the patient_id
+
+tidy_data_day_2 <- tidy_data_day_2%>%
+  arrange(patient_id)
+Additiona_data <- Additiona_data%>%
+  arrange(patient_id)
+glimpse(Additiona_data)
+
+##joining the tidy_data_day_2 and the Additiona_data into one file 
+
+joined_data <- tidy_data_day_2 %>%
+  left_join(Additiona_data, by = "patient_id")
+
+glimpse(joined_data)
+
 
 
